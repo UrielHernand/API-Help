e6ba3801dbc6f360ab19be6ed33af5cad5e22727
Uriel Carrera <a3520110324@alumno.uttehuacan.edu.mx>
2023-11-21 17:09:12 -0600

    Fix database connection configuration

--- src/services/database/index.ts
+++ src/services/database/index.ts
@@ -3,12 +3,14 @@ import 'dotenv/config';
 
 
     const connection =  mysql.createConnection({
+
         host: process.env.CHAT_AI_DB_HOST,
         user: process.env.CHAT_AI_DB_USER,
         database: process.env.CHAT_AI_DB_NAME,
         password: process.env.CHAT_AI_DB_PASSWORD,
         port: Number(process.env.CHAT_AI_DB_PORT),
         waitForConnections: true,
+
     });
 
 
